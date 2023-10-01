b254c31900d2bc9ee91f857dc8f5681080b95a0b
Hashmi <imran@Hashmis-MacBook-Air.local>
2023-10-02 01:32:31 +0530

    profile card in tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div
-      className="py-8 px-8
-	max-w-sm
-	mx-auto
-	rounded-xl
-	shadow-lg
-	space-y-2
-	sm:py-4
-	sm:items-center
-	sm:space-y-0
-	sm:space-x-6"
-    >
-      <img
-        className="block mx-auto h-24 rounded-full sm:shrink-0"
-        src="https://randomuser.me/api/portraits/men/19.jpg"
-        alt="profile"
-      />
-      <div className="text-center space-y-2 sm:text-left">
-        <div className="space-y-0.5">
-          <p className="text-lg text-black font-semibold">Mark Jamey</p>
-          <p className="text-slate-500 font-medium">Product Enginner</p>
-        </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent">
-          Message
-        </button>
+    <div className="max-w-sm py-2 px-2 shadow-lg rounded-lg bg-purple-400 text-white md:max-w-2xl">
+      <div className="flex flex-col justify-center items-center">
+        <img
+          className="rounded-sm w-50 object-cover"
+          src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1528&q=80"
+          alt="profile"
+        />
+        <h1 className="text-black font-bold text-2xl uppercase tracking-wider">
+          Tailwind
+        </h1>
+        <p>Software Developer</p>
       </div>
     </div>
   );
